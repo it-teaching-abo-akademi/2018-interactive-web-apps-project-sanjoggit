901daa29bcaae1f7e6285b9391696ecdaa496c42
sanjoggit <sanjogstha7@gmail.com>
2019-01-02 22:29:19 +0200

    Updates

---
+++ precache-manifest.11805a6f513b2adab1791d4954fb3e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fe5bc7f45fc7f6e102a",
+    "url": "/2018-interactive-web-apps-project-sanjoggit/static/js/runtime~main.4fe5bc7f.js"
+  },
+  {
+    "revision": "0b591c322c5a11a7d309",
+    "url": "/2018-interactive-web-apps-project-sanjoggit/static/js/main.0b591c32.chunk.js"
+  },
+  {
+    "revision": "d2b5a0579deeed8aab0e",
+    "url": "/2018-interactive-web-apps-project-sanjoggit/static/js/1.d2b5a057.chunk.js"
+  },
+  {
+    "revision": "0b591c322c5a11a7d309",
+    "url": "/2018-interactive-web-apps-project-sanjoggit/static/css/main.d2819b83.chunk.css"
+  },
+  {
+    "revision": "d2b5a0579deeed8aab0e",
+    "url": "/2018-interactive-web-apps-project-sanjoggit/static/css/1.c168661f.chunk.css"
+  },
+  {
+    "revision": "cd3995f5b2aaf47967d8968abf32fbd2",
+    "url": "/2018-interactive-web-apps-project-sanjoggit/index.html"
+  }
+];
